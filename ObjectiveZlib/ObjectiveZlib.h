96f547d54a584ecf21242b30b259af46f260734b
Jeremy Olmsted-Thompson <jeremyot@gmail.com>
2012-01-15 20:13:05 -0800

    Initial Commit

---
+++ ObjectiveZlib/ObjectiveZlib.h
@@ -0,0 +1,13 @@
+//
+//  ObjectiveZlib.h
+//  ObjectiveZlib
+//
+//  Created by Jeremy Olmsted-Thompson on 1/15/12.
+//  Copyright (c) 2012 JOT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ObjectiveZlib : NSObject
+
+@end
